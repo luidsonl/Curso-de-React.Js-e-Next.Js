b3390e977a8d003158f6d4e5f8fa509d1d1e6122
Luidson Lima Santos <luidsonlsti@gmail.com>
2024-02-19 13:21:48 -0300

    update

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
 
   const reverseClass = reverse ? 'App-logo-reverse' : '';
+
+  //componentDIdUpdate - executa toda vez que o componente atualiza
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+  //componentDIdMount - executa 1x
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, []);
+
+  //com dependência - executa toda vez que a dependência mudar
+  useEffect(() => {
+    console.log('Counter mudou para', counter);
+  }, [counter]);
 
   const handleClick = () => {
     setReverse(!reverse);
